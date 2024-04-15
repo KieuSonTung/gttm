f501cf925aca975683ba5c70fe90069672b6a022
tungpth_viettel <tungpth.viettel@gmail.com>
2024-04-15 11:09:42 +0100

    data utils: download and convert to images

--- bash/data/download_youtube.sh
+++ bash/data/download_youtube.sh
@@ -1,1 +1,1 @@
-python3 utils/data/download_youtube.py --dst /home/minhnh/project_drive/CV/GTTMData/external/youtube --link https://youtu.be/pBp95ZV-4Ho?si=bI505kPc5lfcj_77
+python3 utils/data/download_youtube.py --dst $GTTMData_PATH/external/youtube/videos --link https://youtu.be/6_m7bkfRKcE?si=utM7n9Z7XNXUeWqf
