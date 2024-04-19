a26a53fe0a53c1b3f95c938b84ef90714943d52b
KieuSonTung <sontungkieu412@gmail.com>
2024-04-19 05:37:40 +0100

    feat: parse config file into yolo
    Load sample_cfg.yaml into yolo.train

--- src/object-detection/yolo/train.py
+++ src/object-detection/yolo/train.py
@@ -3,20 +3,24 @@ from wandb.integration.ultralytics import add_wandb_callback
 import wandb
 import os
 
+
 # Step 1: Initialize a Weights & Biases run
 wandb.init(project="ultralytics", job_type="training")
 
 # Step 2: Define the YOLOv8 Model and Dataset
 model_name = "yolov8n"
-data_path = "/home/tungks/shared_drive_cv/GTTMData/external/traffic_camera"
-dataset_name = os.path.join(data_path, "data.yaml")
-model = YOLO(f"{model_name}.pt")
+home_dir = os.path.expanduser('~')
+
+cfg_path = f'{home_dir}/gttm/sample_cfg.yaml'
+
+model = YOLO('yolov8n.pt')
+cfg = yaml_to_dict(cfg_path)
 
 # Step 3: Add W&B Callback for Ultralytics
 add_wandb_callback(model, enable_model_checkpointing=True)
 
 # Step 4: Train and Fine-Tune the Model
-results = model.train(project="ultralytics", data=dataset_name, epochs=5, device=0)
+results = model.train(**cfg)
 
 # Step 7: Finalize the W&B Run
 wandb.finish()
