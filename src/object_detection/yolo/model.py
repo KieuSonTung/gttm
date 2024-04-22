736f395d688a474011a6e4a71b7901b2ca690e99
KieuSonTung <sontungkieu412@gmail.com>
2024-04-22 17:56:48 +0100

    fix: change each frame 1s apart

--- src/object_detection/yolo/model.py
+++ src/object_detection/yolo/model.py
@@ -25,40 +25,59 @@ class YOLOObjectDetection():
     def infer(self, model_path: str):
         '''
         model_path (str): path to pretrained model (.pt)
-        source (str): source directory for images or videos
         '''
-        model = YOLO(model_path)
-        cap = cv2.VideoCapture(self.cfg['source'])
+
+        # load custom parameter
         pred_cfg = self.cfg.copy()
         pred_cfg.pop('source', None)
         pred_cfg.pop('conf', None)
+        pred_cfg.pop('save', None)
 
-        i = 0
+        # load model
+        model = YOLO(model_path)
+
+        # load video
+        video_capture = cv2.VideoCapture(self.cfg['source'])
         
-        while cap.isOpened():
-            success, frame = cap.read()
+        # Check if the video file is opened successfully
+        if not video_capture.isOpened():
+            print("Error: Unable to open video file")
+        
+        # Initialize variables
+        frame_count = 0
+        fps = int(video_capture.get(cv2.CAP_PROP_FPS))
+        
+        # Read the video frame by frame
+        while True:
             
-            # if i > 5:
-            #     break
-                
-            if success:
-                # print(f'frame {i} read succeeded')
-                # cv2.imwrite(f'my_video_frame_{i}.png', frame)
-
-                # predict
-                results = model.predict(frame, mode='predict', **pred_cfg)
-
-                tensor = results[0].boxes.data
-                zeros_column = torch.zeros(tensor.shape[0], 1, device=tensor.device)
-                zeros_column.fill_(i)
-                new_tensor = torch.cat((zeros_column, tensor), dim=1)
-
-                # concat output tensor of each frame
-                if i == 0:
-                    stacked_tensor = new_tensor
-                else:
-                    stacked_tensor = torch.cat((stacked_tensor, new_tensor), dim=0)
-                
-                i += 1
+            video_capture.set(cv2.CAP_PROP_POS_FRAMES, frame_count * fps)
+            # Read a single frame
+            ret, frame = video_capture.read()
+            
+            # Break the loop if there are no more frames
+            if not ret:
+                break
+        
+            # print(f'frame {frame_count} read succeeded')
+            # cv2.imwrite(f'test_img/my_video_frame_{frame_count}.png', frame)
+        
+            results = model.predict(frame, mode='predict', save=False)
+        
+            tensor = results[0].boxes.data
+            zeros_column = torch.zeros(tensor.shape[0], 1, device=tensor.device)
+            zeros_column.fill_(frame_count)
+            new_tensor = torch.cat((zeros_column, tensor), dim=1)
+        
+            # concat output tensor of each frame
+            if frame_count == 0:
+                stacked_tensor = new_tensor
+            else:
+                stacked_tensor = torch.cat((stacked_tensor, new_tensor), dim=0)
+                        
+            # Increment frame count
+            frame_count += 1
+        
+        # Release the video capture object
+        video_capture.release()
 
         return stacked_tensor
